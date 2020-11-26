def2b45787758bcf4681b0b9726bae5c504fc238
David Knox <dakdave26@gmail.com>
2020-11-26 00:20:00 -0500

    Single plate posting route added

--- src/routes/plates.ts
+++ src/routes/plates.ts
@@ -20,6 +20,7 @@ class BoilerRouter {
     // Register routes.
     this.router.route("/").get(this.getAllPlates(this));
     this.router.route("/plate/:id").get(this.getPlateById(this));
+    this.router.route("/plate").post(this.postOnePlate(this));
   }
 
   // ---- Generators for route callbacks ---- //
@@ -27,7 +28,7 @@ class BoilerRouter {
     return async (req: Request, res: Response) => {
       try {
         const plates = await router.repository.find();
-        res.send(plates);
+        res.send({ success: true, plates: plates});
       } catch (error) {
         console.log("Caught error:", error);
         res.send({ success: false, error: error });
@@ -39,10 +40,23 @@ class BoilerRouter {
     return async (req: Request, res: Response) => {
       try {
         const plate = await router.repository.findOneOrFail(req.params.id);
-        res.send(plate);
+        res.send({success: true, plate: plate});
+      } catch (error) {
+        console.error("Caught error:", error);
+        res.send({ success: false, error: "Something went wrong. Check your id." });
+      }
+    };
+  }
+
+  postOnePlate(router: BoilerRouter): RouteCallback {
+    return async (req: Request, res: Response) => {
+      try {
+        const plate = await req.body; 
+        const result = router.repository.create(plate);
+        res.send({success: true, plate: result});
       } catch (error) {
         console.log("Caught error:", error);
-        res.send({ success: false, error: error });
+        res.send({ success: false, error: "Something went wrong. Check the plate formatting." });
       }
     };
   }
